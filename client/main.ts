ca3ae92c02b4c772f07e142ff93ef88043e018c2
Máximo Zarza <zmaximo1990@gmail.com>
2018-10-09 09:25:30 -0300

    Chat skeleton

--- client/main.ts
+++ client/main.ts
@@ -5,14 +5,7 @@ socket.onmessage = function (message) {
     let data = JSON.parse(message.data);
     console.log(data);
 
-    let className: string = 'other';
-    let date = getFormatedDate(data.date);
-    let userLabel: string = `${data.user} dijo (${date}):`;
-    if (data.user === user) {
-        className = 'me';
-        userLabel = `Yo dije (${date}):`;
-    }
-    document.getElementById('message_area').innerHTML += '<div> <p>' + userLabel + '</p> <p class="' + className + '">' + data.message + '</p></div>';
+    // TODO: mostrar mensaje de conversación según usuario
 };
 
 function getFormatedDate(value: string) {
@@ -21,7 +14,7 @@ function getFormatedDate(value: string) {
 }
 
 function clearInput(): void {
-    (<HTMLInputElement> document.getElementById('text_data')).value = '';
+    // TODO: limpiar el input
 }
 
 function focus() {
@@ -29,12 +22,7 @@ function focus() {
 }
 
 function sendMessage() {
-    let formIsValid: boolean = document.forms['form'].checkValidity();
-    if (!formIsValid) {
-        alert('¡Ingrese un texto!');
-        focus();
-        return;
-    }
+    // TODO: validar formulario y si no es válido mostrar alerta y hacer foco en el input
     let text = (<HTMLInputElement> document.getElementById('text_data')).value;
     socket.send(JSON.stringify({user: user, text: text}));
     clearInput();
